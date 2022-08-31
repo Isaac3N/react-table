8aa3937c0cda7b72246de787e147865d7f89bf51
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-08-31 23:52:43 +0100

    updated search

--- react-table/src/App.js
+++ react-table/src/App.js
@@ -5,8 +5,15 @@ import { Users } from "./users";
 
 function App() {
 	const [query, setQuery] = useState("");
+	const keys = ["first_name", "last_name", "email"];
+
 	const search = (data) => {
-		return data.filter((item) => item.first_name.toLowerCase().includes(query));
+		return data.filter(
+			(item) =>
+				item.first_name.toLowerCase().includes(query) ||
+				item.last_name.toLowerCase().includes(query) ||
+				item.email.toLowerCase().includes(query)
+		);
 	};
 	return (
 		<div className="app">
