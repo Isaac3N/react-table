0f528205500dfd899b75dfd587b849e8cec26601
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-08-31 23:30:44 +0100

    basic search

--- react-table/src/App.js
+++ react-table/src/App.js
@@ -1,9 +1,29 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./app.css";
+import { Users } from "./users";
 
 function App() {
+	const [query, setQuery] = useState("");
+	// console.log(
+	// 	Users.filter((user) => user.first_name.toLowerCase().includes("fe"))
+	// );
 	return (
-		<div className="App">
-			<input type="text" placeholder="search" className="search" />
+		<div className="app">
+			<input
+				type="text"
+				placeholder="search"
+				className="search"
+				onChange={(e) => setQuery(e.target.value)}
+			/>
+			<ul className="list">
+				{Users.filter((user) =>
+					user.first_name.toLowerCase().includes(query)
+				).map((user) => (
+					<li key={user.id} className="listItem">
+						{user.first_name}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
